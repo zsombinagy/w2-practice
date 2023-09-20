34597a051b952e6bed0460f8c2bf889543176bc2
zsombinagy <zsombinagy.14@gmail.com>
2023-09-20 18:08:17 +0200

    made some function

---
+++ script.js
@@ -0,0 +1,47 @@
+/* function logger(param) {
+    console.log(param)
+}
+ 
+
+
+
+function logTheSumOfTwoNumbers (x, y) {
+
+    console.log(x + y)
+
+}
+
+
+
+
+function sumOfTwoNumbers(x, y) {
+    let sum = 0;
+    sum = x + y;
+
+    return sum;
+}
+
+logTheSumOfTwoNumbers(sumOfTwoNumbers(10, 5), 5) */
+
+
+
+
+calculateAreaOfTheCircle(4)
+
+function calculateNerdSurface (a) {
+    let surface = 0;
+    surface = a * a *6; 
+    console.log("The nerd surface is " + surface + "cm3");
+    return surface;
+
+}
+
+function calculateAreaOfTheCircle (r) {
+    let area = 0;
+    area = Math.PI * r * 2; /* calculate the circle area */
+    area = Math.ceil(area) /* round the value up*/
+    console.log('The circle area is ' + area + "cm2");
+    return area;
+}
+
+
